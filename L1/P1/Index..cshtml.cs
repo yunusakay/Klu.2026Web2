8e2423c6ea9a783908ec65b616236e59b736903f
Yunus Akay <yunusakay44@gmail.com>
2026-02-25 10:24:21 +0300

    Add project files.

---
+++ L1/P1/Index..cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace P1
+{
+    public class Index_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ L2/P1/Pages/03.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace P1.Pages
+{
+    public class _03Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
